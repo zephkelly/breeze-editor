5e796d9618a2cbc13d1490bb7963709118b8bddc
zephkelly <masterevank@gmail.com>
2022-10-15 19:01:56 +1100

    Add line count/scrolling

--- Breeze/BreezeApplication.Designer.cs
+++ Breeze/BreezeApplication.Designer.cs
@@ -24,14 +24,23 @@
 			private void InitializeComponent()
 			{
 			this.TitleBar = new System.Windows.Forms.Panel();
-			this.treeView1 = new System.Windows.Forms.TreeView();
-			this.mainEditorBox = new System.Windows.Forms.RichTextBox();
+			this.titleLabel = new System.Windows.Forms.Label();
 			this.minimiseButton = new System.Windows.Forms.Panel();
 			this.expandButton = new System.Windows.Forms.Panel();
 			this.exitButton = new System.Windows.Forms.Panel();
-			this.icon = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.mainEditorBox = new System.Windows.Forms.RichTextBox();
+			this.editorBoxFocusLabel = new System.Windows.Forms.Label();
+			this.splitContainer = new System.Windows.Forms.SplitContainer();
+			this.numberLineLable = new System.Windows.Forms.Label();
+			this.lineNumberBox = new System.Windows.Forms.RichTextBox();
 			this.TitleBar.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.icon)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+			this.splitContainer.Panel1.SuspendLayout();
+			this.splitContainer.Panel2.SuspendLayout();
+			this.splitContainer.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// TitleBar
@@ -39,10 +48,11 @@
 			this.TitleBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.TitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(37)))), ((int)(((byte)(65)))));
+			this.TitleBar.Controls.Add(this.titleLabel);
 			this.TitleBar.Controls.Add(this.minimiseButton);
 			this.TitleBar.Controls.Add(this.expandButton);
 			this.TitleBar.Controls.Add(this.exitButton);
-			this.TitleBar.Controls.Add(this.icon);
+			this.TitleBar.Controls.Add(this.pictureBox1);
 			this.TitleBar.Location = new System.Drawing.Point(0, 0);
 			this.TitleBar.Name = "TitleBar";
 			this.TitleBar.Size = new System.Drawing.Size(1000, 35);
@@ -51,33 +61,18 @@
 			this.TitleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TitleBar_MouseMove);
 			this.TitleBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TitleBar_MouseUp);
 			// 
-			// treeView1
+			// titleLabel
 			// 
-			this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-			this.treeView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(19)))), ((int)(((byte)(43)))));
-			this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.treeView1.Location = new System.Drawing.Point(0, 35);
-			this.treeView1.Name = "treeView1";
-			this.treeView1.Size = new System.Drawing.Size(182, 565);
-			this.treeView1.TabIndex = 2;
-			// 
-			// mainEditorBox
-			// 
-			this.mainEditorBox.AcceptsTab = true;
-			this.mainEditorBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.mainEditorBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(8)))), ((int)(((byte)(32)))));
-			this.mainEditorBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.mainEditorBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.mainEditorBox.ForeColor = System.Drawing.SystemColors.Menu;
-			this.mainEditorBox.Location = new System.Drawing.Point(188, 41);
-			this.mainEditorBox.Name = "mainEditorBox";
-			this.mainEditorBox.Size = new System.Drawing.Size(805, 552);
-			this.mainEditorBox.TabIndex = 3;
-			this.mainEditorBox.Text = "";
-			this.mainEditorBox.WordWrap = false;
+			this.titleLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.titleLabel.AutoSize = true;
+			this.titleLabel.Font = new System.Drawing.Font("rainyhearts", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.titleLabel.ForeColor = System.Drawing.Color.WhiteSmoke;
+			this.titleLabel.Location = new System.Drawing.Point(63, 9);
+			this.titleLabel.Name = "titleLabel";
+			this.titleLabel.Size = new System.Drawing.Size(63, 18);
+			this.titleLabel.TabIndex = 5;
+			this.titleLabel.Text = "Breeze";
+			this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// minimiseButton
 			// 
@@ -89,7 +84,6 @@
 			this.minimiseButton.Name = "minimiseButton";
 			this.minimiseButton.Size = new System.Drawing.Size(45, 35);
 			this.minimiseButton.TabIndex = 4;
-			this.minimiseButton.Paint += new System.Windows.Forms.PaintEventHandler(this.minimiseButton_Paint);
 			this.minimiseButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MinimiseButton_MouseDown);
 			this.minimiseButton.MouseEnter += new System.EventHandler(this.MinimiseButton_MouseEnter);
 			this.minimiseButton.MouseLeave += new System.EventHandler(this.MinimiseButton_MouseExit);
@@ -120,51 +114,155 @@
 			this.exitButton.Name = "exitButton";
 			this.exitButton.Size = new System.Drawing.Size(45, 35);
 			this.exitButton.TabIndex = 2;
-			this.exitButton.Paint += new System.Windows.Forms.PaintEventHandler(this.exitButton_Paint);
 			this.exitButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ExitButton_MouseDown);
 			this.exitButton.MouseEnter += new System.EventHandler(this.ExitButton_MouseEnter);
 			this.exitButton.MouseLeave += new System.EventHandler(this.ExitButton_MouseLeave);
 			// 
-			// icon
+			// pictureBox1
 			// 
-			this.icon.Image = global::Breeze.Properties.Resources.icon;
-			this.icon.Location = new System.Drawing.Point(6, 6);
-			this.icon.Name = "icon";
-			this.icon.Size = new System.Drawing.Size(24, 24);
-			this.icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.icon.TabIndex = 2;
-			this.icon.TabStop = false;
+			this.pictureBox1.Image = global::Breeze.Properties.Resources.icon;
+			this.pictureBox1.Location = new System.Drawing.Point(4, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(52, 35);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// treeView1
+			// 
+			this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.treeView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(13)))), ((int)(((byte)(33)))));
+			this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.treeView1.Location = new System.Drawing.Point(0, 35);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(182, 589);
+			this.treeView1.TabIndex = 2;
+			// 
+			// mainEditorBox
+			// 
+			this.mainEditorBox.AcceptsTab = true;
+			this.mainEditorBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.mainEditorBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(25)))), ((int)(((byte)(51)))));
+			this.mainEditorBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.mainEditorBox.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.mainEditorBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+			this.mainEditorBox.Location = new System.Drawing.Point(2, 0);
+			this.mainEditorBox.Name = "mainEditorBox";
+			this.mainEditorBox.Size = new System.Drawing.Size(755, 574);
+			this.mainEditorBox.TabIndex = 3;
+			this.mainEditorBox.Text = "";
+			this.mainEditorBox.WordWrap = false;
+			this.mainEditorBox.VScroll += new System.EventHandler(this.MainEditor_VerticalChanged);
+			this.mainEditorBox.TextChanged += new System.EventHandler(this.MainEditorBox_TextChanged);
+			// 
+			// editorBoxFocusLabel
+			// 
+			this.editorBoxFocusLabel.AutoSize = true;
+			this.editorBoxFocusLabel.Location = new System.Drawing.Point(185, 587);
+			this.editorBoxFocusLabel.Name = "editorBoxFocusLabel";
+			this.editorBoxFocusLabel.Size = new System.Drawing.Size(0, 13);
+			this.editorBoxFocusLabel.TabIndex = 4;
+			// 
+			// splitContainer
+			// 
+			this.splitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.splitContainer.IsSplitterFixed = true;
+			this.splitContainer.Location = new System.Drawing.Point(190, 43);
+			this.splitContainer.Name = "splitContainer";
+			// 
+			// splitContainer.Panel1
+			// 
+			this.splitContainer.Panel1.AutoScroll = true;
+			this.splitContainer.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(25)))), ((int)(((byte)(51)))));
+			this.splitContainer.Panel1.Controls.Add(this.numberLineLable);
+			this.splitContainer.Panel1.Controls.Add(this.lineNumberBox);
+			this.splitContainer.Panel1.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			// 
+			// splitContainer.Panel2
+			// 
+			this.splitContainer.Panel2.AutoScroll = true;
+			this.splitContainer.Panel2.Controls.Add(this.mainEditorBox);
+			this.splitContainer.Panel2.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.splitContainer.Size = new System.Drawing.Size(803, 574);
+			this.splitContainer.SplitterDistance = 42;
+			this.splitContainer.TabIndex = 5;
+			// 
+			// numberLineLable
+			// 
+			this.numberLineLable.AutoSize = true;
+			this.numberLineLable.ForeColor = System.Drawing.Color.Gray;
+			this.numberLineLable.Location = new System.Drawing.Point(13, 0);
+			this.numberLineLable.Name = "numberLineLable";
+			this.numberLineLable.Size = new System.Drawing.Size(0, 16);
+			this.numberLineLable.TabIndex = 1;
+			this.numberLineLable.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// lineNumberBox
+			// 
+			this.lineNumberBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.lineNumberBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(25)))), ((int)(((byte)(51)))));
+			this.lineNumberBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.lineNumberBox.ForeColor = System.Drawing.Color.Gray;
+			this.lineNumberBox.Location = new System.Drawing.Point(0, 0);
+			this.lineNumberBox.Name = "lineNumberBox";
+			this.lineNumberBox.ReadOnly = true;
+			this.lineNumberBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.lineNumberBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+			this.lineNumberBox.Size = new System.Drawing.Size(42, 574);
+			this.lineNumberBox.TabIndex = 6;
+			this.lineNumberBox.Text = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n" +
+    "31\n32\n33";
+			this.lineNumberBox.WordWrap = false;
 			// 
 			// Breeze
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(8)))), ((int)(((byte)(32)))));
-			this.ClientSize = new System.Drawing.Size(1000, 600);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(25)))), ((int)(((byte)(51)))));
+			this.ClientSize = new System.Drawing.Size(1000, 624);
 			this.ControlBox = false;
-			this.Controls.Add(this.mainEditorBox);
+			this.Controls.Add(this.splitContainer);
+			this.Controls.Add(this.editorBoxFocusLabel);
 			this.Controls.Add(this.treeView1);
 			this.Controls.Add(this.TitleBar);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "Breeze";
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Load += new System.EventHandler(this.Breeze_Load);
 			this.TitleBar.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.icon)).EndInit();
+			this.TitleBar.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.splitContainer.Panel1.ResumeLayout(false);
+			this.splitContainer.Panel1.PerformLayout();
+			this.splitContainer.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+			this.splitContainer.ResumeLayout(false);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 			}
 
 		#endregion
 		private System.Windows.Forms.Panel TitleBar;
-		    private System.Windows.Forms.PictureBox icon;
 		    private System.Windows.Forms.Panel expandButton;
 		    private System.Windows.Forms.Panel exitButton;
 		    private System.Windows.Forms.Panel minimiseButton;
 		private System.Windows.Forms.TreeView treeView1;
 		private System.Windows.Forms.RichTextBox mainEditorBox;
+		private System.Windows.Forms.Label editorBoxFocusLabel;
+		private System.Windows.Forms.SplitContainer splitContainer;
+		private System.Windows.Forms.Label numberLineLable;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label titleLabel;
+		private System.Windows.Forms.RichTextBox lineNumberBox;
 	}
 }
 
--- Breeze/BreezeApplication.cs
+++ Breeze/BreezeApplication.cs
@@ -7,10 +7,13 @@ using System.Drawing;
 using System.Linq;
 using System.Numerics;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Breeze
 {
@@ -23,6 +26,10 @@ namespace Breeze
 			this.DoubleBuffered = true;
 			this.FormBorderStyle = FormBorderStyle.None;
 			this.SetStyle(ControlStyles.ResizeRedraw, true);
+
+			initialCharacterColour = Color.FromArgb(235, 235, 235);
+
+			ChangeTabsWidth(2);
 		}
 
 		#region Resizable Form
@@ -60,6 +67,23 @@ namespace Breeze
 					m.Result = (IntPtr)1;
 					return;
 				}
+			}
+
+			if (m.Msg == WM_MOUSEWHEEL)
+			{
+				int scrollLines = SystemInformation.MouseWheelScrollLines;
+				for (int i = 0; i < scrollLines; i++)
+				{
+					if ((int)m.WParam > 0)
+					{
+						SendMessage(this.Handle, WM_VSCROLL, (IntPtr)0, IntPtr.Zero); // scroll up
+					}
+					else
+					{
+						SendMessage(this.Handle, WM_VSCROLL, (IntPtr)1, IntPtr.Zero); // else scroll down
+					}
+				}
+				return;
 			}
 			base.WndProc(ref m);
 		}
@@ -175,19 +199,159 @@ namespace Breeze
 		#endregion
 		#endregion
 
-		private void Breeze_Load(object sender, EventArgs e)
+		#region Syntax Highlighting
+		Color initialCharacterColour = new Color();
+
+		//Expression patterns
+		string keywords = @"\b(public|private|partial|static|void|class|namespace|interface|enum|using|foreach|in|if|else|do|while)\b";
+		string variables = @"\b(var|const|int|string|long|bool|float|char|this)\b";
+		string comments = @"(/\*[^*]*\*+(?:[^/*][^*]*\*+)*/|\/\/.+?$)";
+		string types = @"\b(Console)\b";
+		string strings = "\".+?\"";
+
+		private void MainEditorBox_TextChanged(object sender, EventArgs e)
 		{
-			
+			mainEditorBox.ForeColor = Color.White;
+
+			//Character collections
+			MatchCollection keywordMatches = Regex.Matches(mainEditorBox.Text, keywords);
+			MatchCollection variableMatches = Regex.Matches(mainEditorBox.Text, variables);
+			MatchCollection stringsMatches = Regex.Matches(mainEditorBox.Text, strings);
+			MatchCollection commentMatches = Regex.Matches(mainEditorBox.Text, comments, RegexOptions.Multiline);
+			MatchCollection typeMatches = Regex.Matches(mainEditorBox.Text, types);
+
+			//Inital properties
+			int initialIndex = mainEditorBox.SelectionStart;
+			int initalLength = mainEditorBox.SelectionLength;
+
+			//Blinking workaround, focus label
+			editorBoxFocusLabel.Focus();
+
+			//Set editor box properties
+			mainEditorBox.SelectionStart = 0;
+			mainEditorBox.SelectionLength = mainEditorBox.Text.Length;
+
+			//Scanning characters
+			foreach (Match match in keywordMatches) {
+				mainEditorBox.SelectionStart = match.Index;
+				mainEditorBox.SelectionLength = match.Length;
+				mainEditorBox.SelectionColor = Color.Cyan;
+			}
+
+			foreach (Match match in variableMatches) {
+				mainEditorBox.SelectionStart = match.Index;
+				mainEditorBox.SelectionLength = match.Length;
+				mainEditorBox.SelectionColor = Color.LightCoral;
+			}
+
+			foreach (Match match in commentMatches) {
+				mainEditorBox.SelectionStart = match.Index;
+				mainEditorBox.SelectionLength = match.Length;
+				mainEditorBox.SelectionColor = Color.Green;
+			}
+
+			foreach (Match match in typeMatches) {
+				mainEditorBox.SelectionStart = match.Index;
+				mainEditorBox.SelectionLength = match.Length;
+				mainEditorBox.SelectionColor = Color.Brown;
+			}
+
+			foreach (Match match in stringsMatches) {
+				mainEditorBox.SelectionStart = match.Index;
+				mainEditorBox.SelectionLength = match.Length;
+				mainEditorBox.SelectionColor = Color.Green;
+			}
+
+			mainEditorBox.SelectionStart = initialIndex;
+			mainEditorBox.SelectionLength = initalLength;
+			mainEditorBox.SelectionColor = initialCharacterColour;
+
+			//Refocus editorbox
+			mainEditorBox.Focus();
+
+			UpdateLineLength();
+		}
+		#endregion
+
+		#region Line count
+		StringBuilder lineCountBuilder = new StringBuilder(100);
+		private void UpdateLineLength()
+		{
+			lineCountBuilder.Clear();
+
+			if (mainEditorBox.Lines.Count() <= 0)
+			{
+				lineNumberBox.Text = "1";
+				return;
+			}
+
+			int lineCount = mainEditorBox.Lines.Count();
+
+			for (int i = 0; i < lineCount; i++)
+			{
+				lineCountBuilder.AppendLine((i+1).ToString());
+			}
+
+			lineNumberBox.Text = lineCountBuilder.ToString();
+			UpdateLineNumberScrolling();
+		}
+		#endregion
+
+		#region Line count scrolling
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		private static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+		private const int WM_MOUSEWHEEL = 0x20A;
+		private const int WM_VSCROLL = 0x115;
+
+		private void MainEditor_VerticalChanged(object sender, EventArgs e)
+		{
+			UpdateLineNumberScrolling();
 		}
 
-		private void minimiseButton_Paint(object sender, PaintEventArgs e)
+		private void UpdateLineNumberScrolling()
 		{
+			int topCharIndex = mainEditorBox.GetCharIndexFromPosition(new Point(0, 10));
 
+			int bottomCharIndex = mainEditorBox.GetCharIndexFromPosition(new Point(
+				mainEditorBox.ClientSize.Width,
+				mainEditorBox.ClientSize.Height
+			));
+
+			int displayedTopline = mainEditorBox.GetLineFromCharIndex(topCharIndex);
+
+			//UpdateLineLength(); //incase line char index is -1
+
+			Console.WriteLine(displayedTopline);
+
+			lineNumberBox.SelectionStart = lineNumberBox.GetFirstCharIndexFromLine(displayedTopline);
+			lineNumberBox.ScrollToCaret();
 		}
+		#endregion
 
-		private void exitButton_Paint(object sender, PaintEventArgs e)
+		#region Tabs settings
+		public void ChangeTabsWidth(int newWidth)
 		{
-
+			switch (newWidth)
+			{
+				case 1:
+					mainEditorBox.SelectionTabs = new int[] { 20 };
+					break;
+				case 2:
+					mainEditorBox.SelectionTabs = new int[] { 20, 40 };
+					break;
+				case 3:
+					mainEditorBox.SelectionTabs = new int[] { 20, 40, 60 };
+					break;
+				case 4:
+					mainEditorBox.SelectionTabs = new int[] { 20, 40, 60, 80 };
+					break;
+				default:
+					Console.WriteLine("Error, tabs width not supported. Setting tab size to 2");
+					mainEditorBox.SelectionTabs = new int[] { 20 };
+					break;
+			}
 		}
+		#endregion
 	}
-}
+}   
